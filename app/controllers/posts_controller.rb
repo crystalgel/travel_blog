1749cb83108ac5e543603043a7901ba2e4f4eddb
Crystal <crystalsmurray@gmail.com>
2015-11-03 13:42:07 -0800

    add validations to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,18 @@ def index
 end
 
 def new
+	@post= Post.new
 end
 
 def create
 	@post = Post.new(post_params)
-	@post.save
+
+	if @post.save
 
 	redirect_to@post
+else
+	render 'new'
+end
 end
 
 def show
