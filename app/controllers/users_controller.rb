85697af5f251012545a3548eb7321dd30d269f80
Crystal <crystalsmurray@gmail.com>
2015-11-04 11:39:17 -0800

    add login logout

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+      @users= User.all
+  end
+
+  def create
+     @user = User.new(user_params)
+     if user.save
+         session[:user_id] = user.id
+     redirect_to root_path
+    else
+    redirect_to root_path
+    end
+end
+
+  def show
+  end
+
+  def new
+      @user = User.new
+  end
+
+
+  private
+  def user_params
+      params.require(:user).permit(:name, :email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-    resources :posts do
-        resources :comments
-end
-        root "posts#index"
+ 
+  root "posts#index"
+  resources :users
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy', as: :logout
 
+  resources :posts do
+      resources :comments
+    end
 end
